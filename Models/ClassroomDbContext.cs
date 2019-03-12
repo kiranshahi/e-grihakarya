f336e5cb18972b4791aa3f37ff9ed3763f9fa007
Kiran Shahi <kiran.shahi.np@gmail.com>
2019-03-12 06:48:21 +0545

    updated.' '

---
+++ Models/ClassroomDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Classroom.Models
+{
+    public class ClassroomDbContext : DbContext
+    {
+        public ClassroomDbContext(DbContextOptions<ClassroomDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ClassDetail> ClassDetails { get; set; }
+    }
+}
