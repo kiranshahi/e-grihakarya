d875613671b9434f1bf07bb42dde268a8bb65ab4
Kiran Shahi <kiran.shahi.np@gmail.com>
2019-03-12 21:56:52 +0545

    login component updated.

--- ClientApp/src/app/login/login.component.ts
+++ ClientApp/src/app/login/login.component.ts
@@ -26,7 +26,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
-      uesrname: ['', Validators.required],
+      username: ['', Validators.required],
       password: ['', Validators.required]
     });
 
