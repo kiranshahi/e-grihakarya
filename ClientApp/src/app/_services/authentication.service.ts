0c03bd6b128e349d2a9a0c1dc61e36a6d6445720
Kiran Shahi <kiran.shahi.np@gmail.com>
2019-03-12 23:02:07 +0545

    authentication service updated.

--- ClientApp/src/app/_services/authentication.service.ts
+++ ClientApp/src/app/_services/authentication.service.ts
@@ -20,7 +20,7 @@ export class AuthenticationService {
   }
 
   login(username: string, password: string) {
-    return this.http.post<any>(`${config.apiUrl}/users/authenticate`, { username, password })
+    return this.http.post<any>('/api/users/authenticate', { username, password })
       .pipe(map(user => {
         // login successful if there's a jwt token in the response
         if (user && user.token) {
