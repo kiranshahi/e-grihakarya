9987ca05648570809c2adeadb3d6b0f99509b4af
Kiran Shahi <kiran.shahi.np@gmail.com>
2019-03-14 07:52:54 +0545

    login component updated.

--- CAS.UI/API/UsersController.cs
+++ CAS.UI/API/UsersController.cs
@@ -1,4 +1,4 @@
-﻿//using Classroom.Entities;
+﻿using CAS.Domain;
 using Classroom.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -14,39 +14,39 @@ namespace Classroom.Controllers
         {
             _userService = userService;
         }
-        //[AllowAnonymous]
-        //[HttpPost("authenticate")]
-        //public IActionResult Authenticate([FromBody]User userParam)
-        //{
-        //    var user = _userService.Authenticate(userParam.Username, userParam.Password);
-        //    if (user==null)
-        //    return BadRequest(new { message = "Username or password is incorrect"});
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]User userParam)
+        {
+            var user = _userService.Authenticate(userParam.Username, userParam.Password);
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
 
-        //    return Ok(user);
-        //}
+            return Ok(user);
+        }
 
-        //[Authorize(Roles = Role.Admin)]
-        //[HttpGet]
-        //public IActionResult GetAll()
-        //{
-        //    var users = _userService.GetAll();
-        //    return Ok(users);
-        //}
-        //[HttpGet("{id}")]
-        //public IActionResult GetById(int id)
-        //{
-        //    var user = _userService.GetById(id);
-        //    if (user == null)
-        //    {
-        //        return NotFound();
-        //    }
+        [Authorize(Roles = Role.Admin)]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var user = _userService.GetById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
 
-        //    var currentUserId = int.Parse(User.Identity.Name);
-        //    if (id != currentUserId && !User.IsInRole(Role.Admin))
-        //    {
-        //        return Forbid();
-        //    }
-        //    return Ok(user);
-        //}
+            var currentUserId = int.Parse(User.Identity.Name);
+            if (id != currentUserId && !User.IsInRole(Role.Admin))
+            {
+                return Forbid();
+            }
+            return Ok(user);
+        }
     }
 }
