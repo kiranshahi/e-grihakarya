bd5c8fa75190902a233a5ed523dbe2714eb35bea
Kiran Shahi <kiran.shahi.np@gmail.com>
2019-03-14 08:12:12 +0545

    api link updated.

--- CAS.UI/ClientApp/src/app/_helpers/error.interceptor.ts
+++ CAS.UI/ClientApp/src/app/_helpers/error.interceptor.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { AuthenticationService } from '../_services/authentication.service';
-import { Observable } from 'rxjs';
-import { _throw as throwError } from 'rxjs/observable/throw';
+import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
   constructor(private authenticationService: AuthenticationService) { }
+
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError(err => {
-      if ([401, 403].indexOf(err.status)!==-1) {
+      if (err.status === 401) {
         // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
         this.authenticationService.logout();
         location.reload(true);
--- CAS.UI/ClientApp/src/app/_services/user.service.ts
+++ CAS.UI/ClientApp/src/app/_services/user.service.ts
@@ -6,9 +6,9 @@ import { User } from "../_models/user";
 export class UserService {
   constructor(private http: HttpClient) { }
   getAll() {
-    return this.http.get<User[]>(`${config.apiUrl}/users`);
+    return this.http.get<User[]>("/api/users");
   }
   getById(id: number) {
-    return this.http.get<User>(`${config.apiUrl}/users/${id}`);
+    return this.http.get<User>(`/api/users/${id}`);
   }
 }
