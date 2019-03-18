da0398c3d413de9722c597141be51461555c5287
Kiran Shahi <kiran.shahi.np@gmail.com>
2019-03-18 17:30:20 +0545

    AssignmentController updated.

--- CAS/Migrations/20190318100049_DatabaseUpdate.Designer.cs
+++ CAS/Migrations/20190318100049_DatabaseUpdate.Designer.cs
@@ -4,14 +4,16 @@ using CAS;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CAS.Migrations
 {
     [DbContext(typeof(CASContext))]
-    partial class CASContextModelSnapshot : ModelSnapshot
+    [Migration("20190318100049_DatabaseUpdate")]
+    partial class DatabaseUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
