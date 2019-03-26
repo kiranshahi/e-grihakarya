e7677dd15bee5ec1418185d1fc95dda467841901
Kiran Shahi <kiran.shahi.np@gmail.com>
2019-03-26 22:16:22 +0545

    class list made dynamic.

--- CAS/ClientApp/src/app/home/home.component.ts
+++ CAS/ClientApp/src/app/home/home.component.ts
@@ -3,22 +3,32 @@ import { User } from '../_models/user';
 import { UserService } from '../_services/user.service';
 import { first } from 'rxjs/operators';
 import { AuthenticationService } from '../_services/authentication.service';
+import { CASClass } from '../_models/casclass';
+import { ClassService } from '../_services/class.service';
 
 @Component({
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-  currentUser: User;
-  userFromApi: User;
+  public currentUser: User;
+  public userFromApi: User;
+  public classes: CASClass[] = [];
   constructor(
     private userService: UserService,
-    private authenticationService: AuthenticationService
+    private authenticationService: AuthenticationService,
+    private classService: ClassService
   ) {
     this.currentUser = this.authenticationService.currentUserValue;
   }
   ngOnInit() {
     this.userService.getById(this.currentUser.id).pipe(first()).subscribe(user => {
       this.userFromApi = user;
+    });
+    this.bindData();
+  }
+  bindData() {
+    this.classService.getAll().subscribe(res => {
+      this.classes = res as CASClass[];
     });
   }
 }
