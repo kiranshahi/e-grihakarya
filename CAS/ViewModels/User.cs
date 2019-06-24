b00138d128263faf3d82d96b6a23df0eb1379c18
Kiran Shahi <kiran.shahi.np@gmail.com>
2019-06-24 11:54:47 +0545

    models updated.

--- CAS/ViewModels/User.cs
+++ CAS/ViewModels/User.cs
@@ -1,10 +1,18 @@
-﻿namespace CAS
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CAS
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
+        [Display(Name ="First Name")]
         public string FirstName { get; set; }
+        [Display(Name ="Middle Name")]
+        public string MiddleName { get; set; }
+        [Display(Name ="Last Name")]
         public string LastName { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
