0cfd4f40078ae9a5c4c8edeaad960c18ac612252
Kiran Shahi <kiran.shahi.np@gmail.com>
2019-08-26 15:04:41 +0545

    update

--- API/UploadController.cs
+++ API/UploadController.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.Http.Headers;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace egrihakarya
