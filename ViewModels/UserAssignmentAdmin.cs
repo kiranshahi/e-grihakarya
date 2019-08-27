d8b348f6ed26feac491d3d6329c5f681e9c53236
Kiran Shahi <kiran.shahi.np@gmail.com>
2019-08-27 17:35:35 +0545

    Key attribute removed

--- ViewModels/UserAssignmentAdmin.cs
+++ ViewModels/UserAssignmentAdmin.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace egrihakarya
 {
     [NotMapped]
     public class UserAssignmentAdmin
     {
-        [Key]
         public int ID { get; set; }
         public int UserID { get; set; }
         public int AssignmentID { get; set; }
