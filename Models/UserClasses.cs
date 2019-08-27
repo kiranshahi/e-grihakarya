cde05e41ee8a46520f09c52c82274289e53291eb
Kiran Shahi <kiran.shahi.np@gmail.com>
2019-08-27 17:51:05 +0545

    Key attribute added on UserClasses models.

--- Models/UserClasses.cs
+++ Models/UserClasses.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace egrihakarya
 {
     public partial class UserClasses
     {
+        [Key]
         public int UserClassId { get; set; }
         public int UserId { get; set; }
         public int ClassId { get; set; }
